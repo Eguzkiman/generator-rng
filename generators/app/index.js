4df5fb647511cd062996f694765a8b0351bcf2b3
Eguzki <eguzki@lacueva.io>
2018-06-24 14:35:37 -0500

    Now adds .editorconfig & rn-cli-config.js to project

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,35 +4,39 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the superb ${chalk.red('generator-rng')} generator!`)
-    );
+	prompting() {
+		// Have Yeoman greet the user.
+		this.log(
+			yosay(`Welcome to the superb ${chalk.red('generator-rng')} generator!`)
+		);
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+		const prompts = [
+			// {
+			// 	type: 'confirm',
+			// 	name: 'someAnswer',
+			// 	message: 'Would you like to enable this option?',
+			// 	default: true
+			// }
+		];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+		return this.prompt(prompts).then(props => {
+			// To access props later use this.props.someAnswer;
+			this.props = props;
+		});
+	}
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+	writing() {
+		this.fs.copy(
+			this.templatePath('rn-cli-config.js'),
+			this.destinationPath('rn-cli-config.js')
+		);
+		this.fs.copy(
+			this.templatePath('.editorconfig'),
+			this.destinationPath('.editorconfig')
+		);
+	}
 
-  install() {
-    this.installDependencies();
-  }
+	// install() {
+	//   this.installDependencies();
+	// }
 };
